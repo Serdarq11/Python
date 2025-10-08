5f89869b5e20d42339aabe6e023e579d67b19385
Serdarq11 <serdarkaya.q@gmail.com>
2025-10-08 16:44:34 +0300

    added arithmetic

---
+++ arithmetic.py
@@ -0,0 +1,12 @@
+# ARITHMETIC =  + addition
+#               - subtraction
+#               * multiplication
+#               / division
+#               // integer division
+#               % remainder
+
+friends = 10
+
+friends = friends // 3          # 3 or friends //= 3
+
+print(friends)
