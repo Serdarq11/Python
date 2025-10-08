4fe9c85fd4d2acb565b14e9912df10f4cfefaf7c
Serdarq11 <serdarkaya.q@gmail.com>
2025-10-08 16:35:41 +0300

    added variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+#This is my first python program
+print('I like coding')
+
+full_name = 'Serdar Kaya'
+age = 22
+gpa = 3.36
+is_student = True
+
+print(f"Hello {full_name}")
+print(f"You are {age} years old.")
+print(f"Your gpa is: {gpa}")
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
